424c3fdde6f7fcec640d3bd1a7d2a6ec15cf630e
EvanilsonP <pevanilson99@gmail.com>
2023-07-24 13:46:52 -0300

    Wrap-up Generating password

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const passInput = document.querySelector('#inputPasswordId');
-const lenInput = document.querySelector('#inputLengthId');
+const passInput = document.querySelector("#inputPasswordId");
+const lenInput = document.querySelector("#inputLengthId");
 const infoLength = document.querySelector('label[for="inputLengthId"]');
-const btnGerar = document.querySelector('#btnGerar');
+const btnGerar = document.querySelector("#btnGerar");
 
 const chkLower = document.querySelector("#chkLowerId");
 const chkUpper = document.querySelector("#chkUpperId");
@@ -11,35 +11,48 @@ const chkSymbols = document.querySelector("#chkSymbolsId");
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const symbols = ["!", "@", "#", "$", "%"];
 
-const characters = Array.from(Array(26)).map((_, i) => i + 97);
-const LowerCaseCharacters = characters.map((item) => String.fromCharCode(item));
-const UpperCaseCharacters = LowerCaseCharacters.map((item) => item.toUpperCase());
+const caracters = Array.from(Array(26)).map((_, i) => i + 97);
+const LowercaseCaracters = caracters.map((item) => String.fromCharCode(item));
+const UppercaseCaracters = LowercaseCaracters.map((item) => item.toUpperCase());
 
 infoLength.innerHTML = lenInput.value;
 
 lenInput.addEventListener("change", () => {
-    infoLength.innerHTML = lenInput.value;
+  infoLength.innerHTML = lenInput.value;
 });
 
 btnGerar.addEventListener("click", () => {
-    generatePassword(chkNumber.checked, chkSymbols.checked, chkLower.checked, chkUpper.checked, lenInput.checked);
+  generatePassword(
+    chkNumber.checked,
+    chkSymbols.checked,
+    chkLower.checked,
+    chkUpper.checked,
+    lenInput.value
+  );
 });
 
 const generatePassword = (
-    hasNumbers,
-    hasSymbols,
-    hasLowercase,
-    hasUppercase,
-    length
+  hasNumbers,
+  hasSymbols,
+  hasLowercase,
+  hasUppercase,
+  lenght
 ) => {
-    const = newArray = [
-        ...(hasNumbers ? numbers : []),
-        ...(hasSymbols ? symbols : []),
-        ...(hasLowercase ? LowerCaseCharacters : []),
-        ...(hasUppercase ? UpperCaseCharacters : [] )
-    ];
+  const newArray = [
+    ...(hasNumbers ? numbers : []),
+    ...(hasSymbols ? symbols : []),
+    ...(hasLowercase ? LowercaseCaracters : []),
+    ...(hasUppercase ? UppercaseCaracters : []),
+  ];
 
-    if(newArray.length === 0) return;
+  if (newArray.length === 0) return;
 
-    let password = '';
+  let password = "";
+
+  for (let i = 0; i < lenght; i++) {
+    const randomIndex = Math.floor(Math.random() * newArray.length);
+    password += newArray[randomIndex];
+  }
+
+  passInput.value = password;
 };

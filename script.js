d0031ba51181a532897db15aa756f2af63810077
EvanilsonP <pevanilson99@gmail.com>
2023-07-24 13:32:11 -0300

    Changing the infoLength information with addEventListener

---
+++ script.js
@@ -0,0 +1,22 @@
+const passInput = document.querySelector('#inputPasswordId');
+const lenInput = document.querySelector('#inputLengthId');
+const infoLength = document.querySelector('label[for="inputLengthId"]');
+const btnGerar = document.querySelector('#btnGerar');
+
+const chkLower = document.querySelector("#chkLowerId");
+const chkUpper = document.querySelector("#chkUpperId");
+const chkNumber = document.querySelector("#chkNumberId");
+const chkSymbols = document.querySelector("#chkSymbolsId");
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const symbols = ["!", "@", "#", "$", "%"];
+
+const characters = Array.from(Array(26)).map((_, i) => i + 97);
+const lowerCaseCharacters = characters.map((item) => String.fromCharCode(item));
+const uperCaseCharacters = lowerCaseCharacters.map((item) => item.toUpperCase());
+
+infoLength.innerHTML = lenInput.value;
+
+lenInput.addEventListener("change", () => {
+    infoLength.innerHTML = lenInput.value;
+});

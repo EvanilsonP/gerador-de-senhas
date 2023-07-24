9c799f97e2632d2e1664d87ca8982efd9f24bc97
EvanilsonP <pevanilson99@gmail.com>
2023-07-24 13:42:02 -0300

    Creating generatePassword function

--- script.js
+++ script.js
@@ -12,11 +12,34 @@ const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const symbols = ["!", "@", "#", "$", "%"];
 
 const characters = Array.from(Array(26)).map((_, i) => i + 97);
-const lowerCaseCharacters = characters.map((item) => String.fromCharCode(item));
-const uperCaseCharacters = lowerCaseCharacters.map((item) => item.toUpperCase());
+const LowerCaseCharacters = characters.map((item) => String.fromCharCode(item));
+const UpperCaseCharacters = LowerCaseCharacters.map((item) => item.toUpperCase());
 
 infoLength.innerHTML = lenInput.value;
 
 lenInput.addEventListener("change", () => {
     infoLength.innerHTML = lenInput.value;
 });
+
+btnGerar.addEventListener("click", () => {
+    generatePassword(chkNumber.checked, chkSymbols.checked, chkLower.checked, chkUpper.checked, lenInput.checked);
+});
+
+const generatePassword = (
+    hasNumbers,
+    hasSymbols,
+    hasLowercase,
+    hasUppercase,
+    length
+) => {
+    const = newArray = [
+        ...(hasNumbers ? numbers : []),
+        ...(hasSymbols ? symbols : []),
+        ...(hasLowercase ? LowerCaseCharacters : []),
+        ...(hasUppercase ? UpperCaseCharacters : [] )
+    ];
+
+    if(newArray.length === 0) return;
+
+    let password = '';
+};
